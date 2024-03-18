49890a76c857b798d14ce55a4a47e33ea5e6e7e0
Vorpal-sword <maxim.suprunenko.it@gmail.com>
2024-03-18 19:28:39 +0200

    lr10 done

---
+++ asp-lrs/Controllers/ConsultationController.cs
@@ -0,0 +1,30 @@
+﻿using asp_lrs.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace asp_lrs.Controllers
+{
+    public class ConsultationController : Controller
+    {
+        public IActionResult RegisterConsultation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterConsultation(ConsultationRequest model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return RedirectToAction("RegistrationSuccess");
+        }
+
+        public IActionResult RegistrationSuccess()
+        {
+            return View();
+        }
+    }
+}
